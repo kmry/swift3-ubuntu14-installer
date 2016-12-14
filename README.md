b1d2d42c7f95654653acedb5c3fca1d7230c6e97
keygx <xj@MBPR-2.local>
2016-12-14 12:49:10 +0900

    Change to Swift 3.0.2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # swift3-ubuntu14-installer
 
-A script for installing Ubuntu 14.04 release of Swift 3.0.1, suitable for use on Cloud9 etc
+A script for installing Ubuntu 14.04 release of Swift 3.0.2, suitable for use on Cloud9 etc
--- install_swift.sh
+++ install_swift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  install_swift -- this script installs swift-3.0 for ubuntu 14 
+#  install_swift -- this script installs swift-3.0.2 for ubuntu 14 
 #
 
 #
@@ -12,11 +12,11 @@ sudo apt-get install clang libicu-dev
 # download
 mkdir ~/downloads
 cd ~/downloads
-wget "https://swift.org/builds/swift-3.0.1-release/ubuntu1404/swift-3.0.1-RELEASE/swift-3.0.1-RELEASE-ubuntu14.04.tar.gz"
+wget "https://swift.org/builds/swift-3.0.2-release/ubuntu1404/swift-3.0.2-RELEASE/swift-3.0.2-RELEASE-ubuntu14.04.tar.gz"
 #
 # extract 
 mkdir ~/swift
-tar xzf swift-3.0.1-RELEASE-ubuntu14.04.tar.gz -C ~/swift --strip-components 1
+tar xzf swift-3.0.2-RELEASE-ubuntu14.04.tar.gz -C ~/swift --strip-components 1
 
 
 #
